6464a7c0b8518137ccbf7757c708e45e2318caa2
Simon Dunn <simondunn90@gmail.com>
2021-12-21 13:50:30 +0000

    make call to mapbox API

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 require('dotenv').config()
 const request = require('postman-request')
 
-const secretKey = process.env.API_KEY
-const url = `http://api.weatherstack.com/current?access_key=${secretKey}&query=London`
+const weatherKey = process.env.WEATHER_API_KEY
+const weatherUrl = `http://api.weatherstack.com/current?access_key=${weatherKey}&query=London`
+const locationKey = process.env.LOCATION_API_KEY
+const locationUrl = `https://api.mapbox.com/geocoding/v5/mapbox.places/London.json?access_token=${locationKey}&limit=1`
 
-request({url:url, json: true}, (err, response) => {
+request({url:weatherUrl, json: true}, (err, response) => {
     // console.log(response.body.current)
     const data  = response.body.current
     const message = `It is ${data.weather_descriptions[0].toLowerCase()} and the temperature is currently ${data.temperature} degrees but it feels like ${data.feelslike} degrees`
     console.log(message)
+})
+
+request({url:locationUrl, json:true}, (err,response) => {
+    const data = response.body.features[0]
+    const long =  data.center[0]
+    const lat =  data.center[1]
+    console.log(data)
+    console.log(`Longitude: ${long} Latitude: ${lat}`)
 })
