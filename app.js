d4e589f8ac8bb600de8f3ce5957fbe46a300b0db
Simon Dunn <simondunn90@gmail.com>
2021-12-21 11:50:36 +0000

    print weather forecast to console

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const secretKey = process.env.API_KEY
 const url = `http://api.weatherstack.com/current?access_key=${secretKey}&query=London`
 
 request({url:url, json: true}, (err, response) => {
-    console.log(response.body.current)
-    console.log('some text')
+    // console.log(response.body.current)
+    const data  = response.body.current
+    const message = `It is ${data.weather_descriptions[0].toLowerCase()} and the temperature is currently ${data.temperature} degrees but it feels like ${data.feelslike} degrees`
+    console.log(message)
 })
