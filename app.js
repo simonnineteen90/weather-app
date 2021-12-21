afe1408f5eb0f3d2c53be9ce6714874431280c6b
Simon Dunn <simondunn90@gmail.com>
2021-12-21 14:09:37 +0000

    rm gitignore and add standard js

--- app.js
+++ app.js
@@ -6,17 +6,17 @@ const weatherUrl = `http://api.weatherstack.com/current?access_key=${weatherKey}
 const locationKey = process.env.LOCATION_API_KEY
 const locationUrl = `https://api.mapbox.com/geocoding/v5/mapbox.places/London.json?access_token=${locationKey}&limit=1`
 
-request({url:weatherUrl, json: true}, (err, response) => {
-    // console.log(response.body.current)
-    const data  = response.body.current
-    const message = `It is ${data.weather_descriptions[0].toLowerCase()} and the temperature is currently ${data.temperature} degrees but it feels like ${data.feelslike} degrees`
-    console.log(message)
+request({ url: weatherUrl, json: true }, (err, response) => {
+  // console.log(response.body.current)
+  const data = response.body.current
+  const message = `It is ${data.weather_descriptions[0].toLowerCase()} and the temperature is currently ${data.temperature} degrees but it feels like ${data.feelslike} degrees`
+  console.log(message)
 })
 
-request({url:locationUrl, json:true}, (err,response) => {
-    const data = response.body.features[0]
-    const long =  data.center[0]
-    const lat =  data.center[1]
-    console.log(data)
-    console.log(`Longitude: ${long} Latitude: ${lat}`)
+request({ url: locationUrl, json: true }, (err, response) => {
+  const data = response.body.features[0]
+  const long = data.center[0]
+  const lat = data.center[1]
+  console.log(data)
+  console.log(`Longitude: ${long} Latitude: ${lat}`)
 })
