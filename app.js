cf4f021e6a35b402f82e17b3e631df0d773ef159
Simon Dunn <simondunn90@gmail.com>
2021-12-21 15:56:11 +0000

    error handing for API requests

--- app.js
+++ app.js
@@ -4,19 +4,29 @@ const request = require('postman-request')
 const weatherKey = process.env.WEATHER_API_KEY
 const weatherUrl = `http://api.weatherstack.com/current?access_key=${weatherKey}&query=London`
 const locationKey = process.env.LOCATION_API_KEY
-const locationUrl = `https://api.mapbox.com/geocoding/v5/mapbox.places/London.json?access_token=${locationKey}&limit=1`
+const locationUrl = `https://api.mapbox.com/geocoding/v5/mapbox.places/York.json?access_token=${locationKey}&limit=1`
 
 request({ url: weatherUrl, json: true }, (err, response) => {
-  // console.log(response.body.current)
-  const data = response.body.current
-  const message = `It is ${data.weather_descriptions[0].toLowerCase()} and the temperature is currently ${data.temperature} degrees but it feels like ${data.feelslike} degrees`
-  console.log(message)
+  if (err) {
+    console.log(`Ooops something went wrong: ${err}`)
+  } else if (response.body.error) {
+    console.log(`Ooops something went wrong with the response. Error code: ${response.body.error.code}. ${response.body.error.info}`)
+  } else {
+    const data = response.body.current
+    const message = `It is ${data.weather_descriptions[0].toLowerCase()} and the temperature is currently ${data.temperature} degrees but it feels like ${data.feelslike} degrees`
+    console.log(message)
+  }
 })
 
 request({ url: locationUrl, json: true }, (err, response) => {
-  const data = response.body.features[0]
-  const long = data.center[0]
-  const lat = data.center[1]
-  console.log(data)
-  console.log(`Longitude: ${long} Latitude: ${lat}`)
+  if (err) {
+    console.log(`Ooops something went wrong: ${err}`)
+  } else if (response.body.features.length === 0) {
+    console.log('Ooops something went wrong with the response. Location not found.')
+  } else {
+    const data = response.body.features[0]
+    const long = data.center[0]
+    const lat = data.center[1]
+    console.log(`Longitude: ${long} Latitude: ${lat}`)
+  }
 })
